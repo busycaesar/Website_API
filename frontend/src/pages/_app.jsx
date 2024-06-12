cc285ffb1c8e6de4b20ca7d577825df033e41b8b
Dev <busycaesar@gmail.com>
2024-06-12 17:25:30 -0400

    Install(F): React boostrap

---
+++ frontend/src/pages/_app.jsx
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main
+      className={`flex bg-black text-white min-h-screen flex-col items-center ${inter.className}`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
+}
