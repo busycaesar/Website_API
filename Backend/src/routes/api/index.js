fb9c38619cb88d964293f756feb80d5c24367a0d
Dev <busycaesar@gmail.com>
2024-06-20 18:23:35 -0400

    Add(B): Listed API for API Keys

--- Backend/src/routes/api/index.js
+++ Backend/src/routes/api/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 // Authentication APIs
 router.use("/auth", require("./auth"));
 
+// API Keys
+router.use("/apikeys", require("./apiKey"));
+
 module.exports = router;
