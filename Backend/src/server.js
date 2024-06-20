59f32cb33ef68c9a877b35ef1815f5dd2c979f4f
Dev <busycaesar@gmail.com>
2024-06-20 18:22:05 -0400

    Refactor(B): Create all the required tables before starting the server.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,13 @@
 const app = require("./app");
 require("dotenv").config();
+const { createTables } = require("./db");
 
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, async () => {
   try {
+    console.log("Create all the required tables.");
+    await createTables();
     console.log(`Server started on PORT: ${PORT}`);
   } catch (error) {
     console.log(`Could not start the server. ${error}`);
